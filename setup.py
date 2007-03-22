6d5483edba8a84ad97040e2193e531aa3439f905
Jodok Batlogg <jodok@batlogg.com>
2007-03-22 16:54:21 +0000

    added buildout support

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 #!python
 from setuptools import setup, find_packages
 
-setup(name='z3c.traverser',
-      version='0.1.0',
-      author = "Zope Community",
-      author_email = "zope3-dev@zope.org",
-      description = open("README.txt").read(),
-      license = "ZPL 2.1",
-      keywords = "zope zope3",
-      url='http://svn.zope.org//z3c.traverser',
-
-      zip_safe=False,
-      packages=find_packages('src'),
-      include_package_data=True,
-      package_dir = {'':'src'},
-      namespace_packages=['z3c',],
+setup(
+    name = 'z3c.traverser',
+    version = '0.1.0',
+    author = "Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = open("README.txt").read(),
+    license = "ZPL 2.1",
+    keywords = "zope zope3",
+    url='http://svn.zope.org//z3c.traverser',
+    zip_safe=False,
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir = {'':'src'},
+    namespace_packages=['z3c',],
     install_requires=[
-        'setuptools'
-    ],
-     )
+        'setuptools',
+        'zope.component'],
+    extras_require = dict(
+        test = ['zope.app.testing']
+        ),
+)
 
