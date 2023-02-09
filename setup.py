e25a8a38e51d6930eb18cdeeb1fe19afb1b97c57
Michael Howitz <mh@gocept.com>
2023-02-09 12:11:05 +0100

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='z3c.traverser',
-      version='2.0',
+      version='2.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Pluggable Traversers And URL handling utilities',
